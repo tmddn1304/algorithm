4cc50196562c02db6f9c561c4c3eb66f3f4e231d
tmddn1304 <52819382+tmddn1304@users.noreply.github.com>
2020-10-24 17:26:04 +0900

    Create 언어해독기.java

---
+++ 기타/언어해독기.java
@@ -0,0 +1,58 @@
+package nhn;
+
+import java.util.Stack;
+
+public class Solution3 {
+	public static void solution(int numOfOrder, String[] orderArr) {
+ 
+		for(String str : orderArr) {
+			Stack<Character> stack = new Stack<>();
+			for(int i=0; i<str.length(); i++) {
+				stack.add(str.charAt(i));
+			}
+			StringBuilder sb = new StringBuilder();
+			while(!stack.isEmpty()) {
+				Character pop = stack.pop();
+				if(pop == ')') continue;
+				else if(pop =='(') {
+					Character c = stack.pop();
+					if(c >='0' && c <='9') {
+						String temp= sb.toString();
+						int a=Character.getNumericValue(c);
+						while(a>1) {
+							sb.append(temp);
+							a--;
+						}
+					}
+					else {
+						String temp = sb.toString();
+						int length = temp.length();
+						
+						for(int i=0; i<length; i++) {
+							sb.insert((2*i)+1, c);
+						}
+					}
+				}
+				else if(pop >='0' && pop <='9') {
+					int a=Character.getNumericValue(pop);
+					while(a>1) {
+						sb.append(sb.toString().charAt(sb.length()-1));
+						a--;
+					}
+				}
+				else {
+					sb.append(pop);
+				}
+			}
+			System.out.println(sb.reverse().toString());
+		}
+
+	  }
+	public static void main(String[] args) {
+		int numOfOrder=2;
+		String[] orderArr = {"B2(RG)","1B2R3G"};
+		
+		solution(numOfOrder, orderArr);
+	}
+
+}
