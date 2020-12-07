307916edfdcb9352d98ebcab8ae2f47a7205d805
tmddn1304 <52819382+tmddn1304@users.noreply.github.com>
2020-12-07 18:35:44 +0900

    Update 퀵소트.java

--- 기타/퀵소트.java
+++ 기타/퀵소트.java
@@ -3,26 +3,31 @@ package prac;
 import java.util.Arrays;
 
 public class 퀵소트 {
-	public static void sort(int[] data, int l, int r){
-        int left = l;
-        int right = r;
-        int pivot = data[(l+r)/2];
-        
-        do{
-            while(data[left] < pivot) left++;
-            while(data[right] > pivot) right--;
-            if(left <= right){    
-                int temp = data[left];
-                data[left] = data[right];
-                data[right] = temp;
-                left++;
-                right--;
-            }
-        }while (left <= right);
-        
-        if(l < right) sort(data, l, right);
-        if(r > left) sort(data, left, r);
-    }
+	public static void sort(int[] arr, int l, int r){
+		int i, j, pivot, tmp;
+
+		if (r <= l)
+			return;
+
+		i = l;
+		j = r;
+		pivot = arr[r];
+		while (i < j) {
+			while (arr[i] < pivot)
+				i++;
+			while (i < j && arr[j] >= pivot)
+				j--;
+
+			tmp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = tmp;
+		}
+		arr[r] = arr[i];
+		arr[i] = pivot;
+
+		sort(arr, l, i - 1);
+		sort(arr, i + 1, r);
+	}
 
 	
 	public static void main(String[] args) {
